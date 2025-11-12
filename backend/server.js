d8c38abbdbf071e9eb92a027e93c2ae2b5c994c4
Erdi Topuzlu <erditpzl@gmail.com>
2025-11-13 01:33:55 +0300

    port change

--- backend/server.js
+++ backend/server.js
@@ -84,6 +84,6 @@ app.post('/api/reservations', (req,res)=>{
   });
 });
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
